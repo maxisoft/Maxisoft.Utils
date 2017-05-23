14abf8b260394fbe0ef42629c7c8d1521561ef1c
maxisoft <maxisoft.eu@gmail.com>
2017-05-23 14:39:07 +0200

    Added StringUtils and ProcessExtension

---
+++ Maxisoft.Utils/Process/ProcessExtension.cs
@@ -0,0 +1,27 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Maxisoft.Utils.Process
+{
+    public static class ProcessExtension
+    {
+        /// <summary>
+        /// Waits asynchronously for the process to exit.
+        /// </summary>
+        /// <param name="process">The process to wait for cancellation.</param>
+        /// <param name="cancellationToken">A cancellation token. If invoked, the task will return
+        /// immediately as canceled.</param>
+        /// <returns>A Task representing waiting for the process to end.</returns>
+        public static Task WaitForExitAsync(this System.Diagnostics.Process process,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var tcs = new TaskCompletionSource<object>();
+            process.EnableRaisingEvents = true;
+            process.Exited += (sender, args) => tcs.TrySetResult(null);
+            if (cancellationToken != default(CancellationToken))
+                cancellationToken.Register(tcs.SetCanceled);
+
+            return tcs.Task;
+        }
+    }
+}
