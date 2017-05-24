9734fce965fe9947cf718e97b926da025bc52fc8
maxisoft <maxisoft.eu@gmail.com>
2017-05-24 10:29:34 +0200

    Fixed test method StartPingProcess

--- Maxisoft.Utils.Tests/Process/ProcessExtentionTest.cs
+++ Maxisoft.Utils.Tests/Process/ProcessExtentionTest.cs
@@ -15,7 +15,7 @@ namespace Maxisoft.Utils.Tests.Process
         [Fact]
         public async Task TestWaitForExitAsync_Nominal()
         {
-            var process = StartPingProcess();
+            var process = StartPingProcess(1);
             await process.WaitForExitAsync().ToObservable().Timeout(TimeSpan.FromSeconds(5));
             Assert.True(process.HasExited);
         }
@@ -23,7 +23,7 @@ namespace Maxisoft.Utils.Tests.Process
         [Fact]
         public async Task TestWaitForExitAsync_WithToken()
         {
-            var process = StartPingProcess();
+            var process = StartPingProcess(50);
             var cts = new CancellationTokenSource();
             cts.CancelAfter(TimeSpan.FromSeconds(1));
             await Assert.ThrowsAsync<TaskCanceledException>(async () => await process.WaitForExitAsync(cts.Token)
@@ -35,18 +35,18 @@ namespace Maxisoft.Utils.Tests.Process
             Assert.True(process.HasExited);
         }
 
-        private static System.Diagnostics.Process StartPingProcess()
+        private static System.Diagnostics.Process StartPingProcess(int count)
         {
             var info = new ProcessStartInfo();
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 info.FileName = "cmd";
-                info.Arguments = "/c \"ping 127.0.0.1 -n 50\"";
+                info.Arguments = $"/c \"ping 127.0.0.1 -n {count}\"";
             }
             else //assume unix
             {
                 info.FileName = "sh";
-                info.Arguments = "-c \"ping -c 50 127.0.0.1\"";
+                info.Arguments = "-c \"ping -c {count} 127.0.0.1\"";
             }
             info.RedirectStandardOutput = true;
             info.RedirectStandardError = true;
