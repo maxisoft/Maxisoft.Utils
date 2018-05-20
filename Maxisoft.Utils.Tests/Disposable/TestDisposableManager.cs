f7055b64cf6a0b6aa7435a42b60625c09ca78413
maxisoft <maxisoft.eu@gmail.com>
2018-05-20 12:47:14 +0200

    AtExit handle Prepend / Append modifier Disposable manager now handle both weak and strong reference

--- Maxisoft.Utils.Tests/Disposable/TestDisposableManager.cs
+++ Maxisoft.Utils.Tests/Disposable/TestDisposableManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Maxisoft.Utils.Disposable;
 using Moq;
+using Optional.Collections;
 using Xunit;
 
 namespace Maxisoft.Utils.Tests.Disposable
@@ -12,33 +13,39 @@ namespace Maxisoft.Utils.Tests.Disposable
         [Fact]
         public void TestDisposableManager_Basics()
         {
-            var disposables = new Mock<LinkedList<WeakReference<IDisposable>>> {CallBase = true};
+            var disposables = new Mock<LinkedList<OptinalWeakDisposable>> {CallBase = true};
             var dm = new TDisposableManager(disposables.Object);
             var disposable = AddDisposable(dm);
             
             dm.Dispose();
+            // hold the weak ref target object
+            var weakdisposable = AddWeakDisposable(dm);
             
             var count = 0;
             //wait the for the GC to cleanup thing
             //try hard mode
-            while (disposables.Object.Count != 0 && count++ < 500)
+            while (disposables.Object.Count > 1 && count++ < 500)
             {
                 dm.CleanupLinkedDisposable();
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
             }
-            Assert.True(disposables.Object.Count == 0, "there's linked disposables left"); 
+            dm.CleanupLinkedDisposable();
+            Assert.True(disposables.Object.Count == 1, "there's a weak linked disposables left");
+            Assert.True(disposables.Object.FirstOrDefault(r => r == disposable.Object) == default(OptinalWeakDisposable), "there's hard linked disposables left");
+            Assert.True(disposables.Object.FirstOrDefault(r => r == weakdisposable.Object) != default(OptinalWeakDisposable), "there's no weak linked disposables left");
             disposable.Verify(d => d.Dispose(), Times.Once);
+            weakdisposable.Verify(d => d.Dispose(), Times.Never);
         }
         
         [Fact]
         public void TestDisposableManager_WeakRef()
         {
-            var disposables = new Mock<LinkedList<WeakReference<IDisposable>>> {CallBase = true};
+            var disposables = new Mock<LinkedList<OptinalWeakDisposable>> {CallBase = true};
             var dm = new TDisposableManager(disposables.Object);
             //add a disposable via another function scope
             {
-                void Action() => dm.LinkDisposable(new EmptyDisposable());
+                void Action() => dm.LinkDisposable(new WeakReference<IDisposable>(new EmptyDisposable()));
                 Action();
             }
 
@@ -52,7 +59,7 @@ namespace Maxisoft.Utils.Tests.Disposable
                 GC.WaitForPendingFinalizers();
             }
             Assert.False(count >= 500);
-            Assert.Null(disposables.Object.FirstOrDefault(reference => reference.TryGetTarget(out IDisposable tmp) && tmp is EmptyDisposable));
+            Assert.True(disposables.Object.FirstOrDefault(reference => reference.WeakReference != null && reference.WeakReference.TryGetTarget(out var tmp) && tmp is EmptyDisposable) == default(OptinalWeakDisposable));
         }
 
         private static Mock<IDisposable> AddDisposable(IDisposableManager disposableManager)
@@ -61,10 +68,17 @@ namespace Maxisoft.Utils.Tests.Disposable
             disposableManager.LinkDisposable(disposable.Object);
             return disposable;
         }
+        
+        private static Mock<IDisposable> AddWeakDisposable(IDisposableManager disposableManager)
+        {
+            var disposable = new Mock<IDisposable>();
+            disposableManager.LinkDisposable(new WeakReference<IDisposable>(disposable.Object));
+            return disposable;
+        }
 
         private class TDisposableManager : DisposableManager
         {
-            protected internal TDisposableManager(LinkedList<WeakReference<IDisposable>> collection) : base(collection)
+            protected internal TDisposableManager(LinkedList<OptinalWeakDisposable> collection) : base(collection)
             {
             }
         }
