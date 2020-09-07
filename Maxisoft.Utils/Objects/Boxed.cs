d97a04f04101ce319b06c563e9766974daaec421
maxisoft <maxisoft.eu@gmail.com>
2020-09-07 12:51:08 +0200

    Added Boxed class and ObjectExtensions

---
+++ Maxisoft.Utils/Objects/Boxed.cs
@@ -0,0 +1,35 @@
+﻿namespace Maxisoft.Utils.Objects
+{
+    public sealed class Boxed<T>
+    {
+        private T _value;
+
+        public Boxed(in T value)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public static implicit operator T(Boxed<T> boxed)
+        {
+            return boxed.Value;
+        }
+        
+        public static implicit operator Boxed<T>(T value)
+        {
+            return new Boxed<T>(value);
+        }
+
+        public ref T Ref()
+        {
+            return ref _value;
+        }
+
+        public bool IsNull() => ReferenceEquals(null, _value);
+    }
+}
---
+++ Maxisoft.Utils/Objects/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Maxisoft.Utils.Objects
+{
+    public static class ObjectExtensions
+    {
+        public static Boxed<T> Box<T>(in T value) => new Boxed<T>(in value);
+
+        public static T[] WrapIntoArray<T>(in T value) => new[] {value};
+
+        public static WeakReference<T> WeakReference<T>(in T value) where T : class => new WeakReference<T>(value);
+    }
+}
