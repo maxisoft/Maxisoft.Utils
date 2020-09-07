101c18cc55d74682e70c084b17e6b42fd3682c6e
maxisoft <maxisoft.eu@gmail.com>
2020-09-07 12:53:38 +0200

    Added EnumerableExtensions

---
+++ Maxisoft.Utils/Collection/EnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Maxisoft.Utils.Collection
+{
+    public static class EnumerableExtensions
+    {
+        public static Deque<T> ToDeque<T>(this IEnumerable<T> enumerable)
+        {
+            var res = new Deque<T>();
+            foreach (var element in enumerable)
+            {
+                res.PushBack(element);
+            }
+
+            return res;
+        }
+        
+        public static LinkedList<T> ToLinkedList<T>(this IEnumerable<T> enumerable)
+        {
+            var res = new LinkedList<T>();
+            foreach (var element in enumerable)
+            {
+                res.AddLast(element);
+            }
+
+            return res;
+        }
+    }
+}
