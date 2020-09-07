05c7a822caea4c604b5265403917faaca61e58ca
maxisoft <maxisoft.eu@gmail.com>
2020-09-07 20:41:31 +0200

    Added EmptyAction

---
+++ Maxisoft.Utils/Empty/EmptyAction.cs
@@ -0,0 +1,228 @@
+﻿using System;
+
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Maxisoft.Utils.Empty
+{
+    public struct EmptyAction
+    {
+        public static void Call()
+        {
+        }
+
+        public static implicit operator Action(EmptyAction _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1>
+    {
+        public static void Call(T1 _)
+        {
+        }
+
+        public static implicit operator Action<T1>(EmptyAction<T1> _)
+        {
+            return Call;
+        }
+    }
+
+    #region Generated EmptyAction
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11,
+            T12 _12, T13 _13, T14 _14, T15 _15, T16 _16)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11,
+            T12 _12, T13 _13, T14 _14, T15 _15)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11,
+            T12 _12, T13 _13, T14 _14)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11,
+            T12 _12, T13 _13)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11,
+            T12 _12)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10, T11 _11)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9, T10 _10)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8, T9 _9)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8, T9> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7, T8 _8)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7, T8>(
+            EmptyAction<T1, T2, T3, T4, T5, T6, T7, T8> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6, T7>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6, T7 _7)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6, T7>(EmptyAction<T1, T2, T3, T4, T5, T6, T7> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5, T6>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5, T6>(EmptyAction<T1, T2, T3, T4, T5, T6> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4, T5>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4, T5>(EmptyAction<T1, T2, T3, T4, T5> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3, T4>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3, T4 _4)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3, T4>(EmptyAction<T1, T2, T3, T4> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2, T3>
+    {
+        public static void Call(T1 _1, T2 _2, T3 _3)
+        {
+        }
+
+        public static implicit operator Action<T1, T2, T3>(EmptyAction<T1, T2, T3> _)
+        {
+            return Call;
+        }
+    }
+
+    public struct EmptyAction<T1, T2>
+    {
+        public static void Call(T1 _1, T2 _2)
+        {
+        }
+
+        public static implicit operator Action<T1, T2>(EmptyAction<T1, T2> _)
+        {
+            return Call;
+        }
+    }
+
+    #endregion
+}
