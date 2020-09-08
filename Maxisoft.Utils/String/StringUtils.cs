abb58fc46ae4299124c1b0d7ee3e71f60a0e4a89
maxisoft <maxisoft.eu@gmail.com>
2020-09-08 22:54:36 +0200

    Stronger/Safer StringUtils.FormatFromDictionary implementation

--- Maxisoft.Utils/String/StringUtils.cs
+++ Maxisoft.Utils/String/StringUtils.cs
@@ -1,24 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Maxisoft.Utils.String
 {
     public static class StringUtils
     {
-        public static string FormatFromDictionary(this string formatString, Dictionary<string, object> valueDict)
+        public static string FormatFromDictionary<TValue>(this string formatString, Dictionary<string, TValue> valueDict)
         {
             var i = 0;
-            var newFormatString = new StringBuilder(formatString);
-            var keyToInt = new Dictionary<string, int>();
-            foreach (var tuple in valueDict)
+            var f = formatString;
+            var arr = new object[valueDict.Count];
+            foreach (var kv in valueDict)
             {
-                newFormatString = newFormatString.Replace("{" + tuple.Key + "}", "{" + i + "}");
-                keyToInt.Add(tuple.Key, i);
-                i++;
+                var needle = kv.Key;
+                if (needle.Contains('"'))
+                {
+                    throw new ArgumentException("Forbidden character \" found in key");
+                }
+
+                if (needle.Trim('{', '}') != needle)
+                {
+                    throw new ArgumentException("Forbidden character { or } found in key head/tail");
+                }
+                needle = needle.Replace("{", "{{");
+                needle = needle.Replace("}", "}}");
+                var regex = @"({*)(?<!(""|')){\s*(" + Regex.Escape(needle) + @")\s*(:\s*\w+)??}(?!(""|'))(}*)";
+                var matches = Regex.Matches(f, regex);
+                var matchCounter = 0;
+                foreach (Match match in matches)
+                {
+                    if (match.Groups[6].Length != match.Groups[1].Length)
+                    {
+                        throw new ArgumentException($"Unbalanced bracket for {kv.Key} #{matchCounter}");
+                    }
+
+                    if (match.Groups[1].Length % 2 != 0)
+                    {
+                        throw new ArgumentException($"Unbalanced bracket for {kv.Key} #{matchCounter}");
+                    }
+
+                    matchCounter += 1;
+                }
+
+                if (matchCounter > 0)
+                {
+                    var replacement = @"$1{" + i + @"$4}$6";
+                    f = Regex.Replace(f, regex, replacement);
+                    arr[i] = kv.Value;
+                    i++;
+                }
+                
             }
-            return string.Format(newFormatString.ToString(),
-                valueDict.OrderBy(x => keyToInt[x.Key]).Select(x => x.Value).ToArray());
+
+            return string.Format(f, arr);
         }
     }
 }
