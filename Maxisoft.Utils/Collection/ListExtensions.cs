62fe2d39640936a6041ef7727d94c1197434218e
maxisoft <maxisoft.eu@gmail.com>
2020-09-13 23:46:17 +0200

    Added WrappedIndex for python like negative array indexes

---
+++ Maxisoft.Utils/Collection/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maxisoft.Utils.Collection
+{
+    public static class ListExtensions
+    {
+        public static ref T At<T>(this T[] array, WrappedIndex index)
+        {
+            return ref array[index.Resolve(array)];
+        }
+        
+        public static T At<T, TList>(TList list, WrappedIndex index) where TList : IList<T>
+        {
+            return list[index.Resolve<T, TList>(list)];
+        }
+        
+        public static T At<T>(this IList<T> list, WrappedIndex index)
+        {
+            return At<T, IList<T>>(list, index);
+        }
+    }
+}
---
+++ Maxisoft.Utils/Collection/WrappedIndex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Maxisoft.Utils.Collection
+{
+
+    public readonly struct WrappedIndex
+    {
+        public readonly int Value;
+
+        public WrappedIndex(int value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator WrappedIndex(int value)
+        {
+            return new WrappedIndex(value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Resolve(int size)
+        {
+            return Value < 0 ? size + Value : size;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Resolve<T, TCollection>(in TCollection collection) where TCollection: ICollection<T>
+        {
+            return Resolve(collection.Count);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Resolve<TCollection>(in TCollection collection) where TCollection: ICollection
+        {
+            return Resolve(collection.Count);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Resolve(in Array array)
+        {
+            return Resolve(array.Length);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Resolve<T>(in T[] array)
+        {
+            return Resolve(array.Length);
+        }
+    }
+}
