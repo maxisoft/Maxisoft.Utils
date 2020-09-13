f1d9870fde84dddde39e84fa418ef5cc3b854a68
maxisoft <maxisoft.eu@gmail.com>
2020-09-13 23:44:44 +0200

    Added LinkedListAsIList: a linked list which implement IList

---
+++ Maxisoft.Utils/Collection/LinkedListAsIList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Maxisoft.Utils.Collection
+{
+    /// <summary>
+    ///     A linked list which implement <see cref="IList{T}"/>
+    /// </summary>
+    /// <remarks>All <see cref="IList{T}"/> methods added here are in <c>O(n)</c> time</remarks>
+    /// <typeparam name="T"></typeparam>
+    /// <see cref="LinkedList{T}" />
+    /// <see cref="LinkedListExtensions"/>
+    [DebuggerDisplay("Count = {Count}")]
+    [Serializable]
+    public class LinkedListAsIList<T> : LinkedList<T>, IList<T>, IReadOnlyList<T>
+    {
+        public int IndexOf(T item)
+        {
+            return LinkedListExtensions.IndexOf(this, item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            LinkedListExtensions.Insert(this, index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            LinkedListExtensions.RemoveAt(this, index);
+        }
+
+        [SuppressMessage("ReSharper", "InvokeAsExtensionMethod")]
+        public T this[int index]
+        {
+            get => LinkedListExtensions.At(this, index).Value;
+            set => LinkedListExtensions.At(this, index).Value = value;
+        }
+    }
+}
