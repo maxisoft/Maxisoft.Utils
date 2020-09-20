fb75e0a10467f49d7109cdff350e6d0b048480dd
maxisoft <maxisoft.eu@gmail.com>
2020-09-20 10:46:02 +0200

    Added Math2 class utils for missing Math stuff in .net

---
+++ Maxisoft.Utils.Tests/Math2Tests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace Maxisoft.Utils.Tests
+{
+    public class Math2Tests
+    {
+
+        private static ulong NumberOfTrailingZerosSlow(ulong i)
+        {
+            checked
+            {
+                if (i == 0) return 64;
+                if (i < 0) return 0;
+                ulong res = 0;
+                while ((i & 1) == 0)
+                {
+                    if (++res >= 64) return 64;
+                    i >>= 1;
+                }
+
+                return res;
+            }
+            
+        }
+        
+        [Theory]
+        [InlineData(0b010, 1)]
+        [InlineData(0b011, 0)]
+        [InlineData(0b001, 0)]
+        [InlineData(0b1001000, 3)]
+        [InlineData(0b11100100000000L, 8)]
+        public void TestNumberOfTrailingZeros(ulong x, ulong expected)
+        {
+            Assert.Equal(expected, Math2.NumberOfTrailingZeros(x));
+            Assert.Equal(NumberOfTrailingZerosSlow(x), Math2.NumberOfTrailingZeros(x));
+        }
+    }
+}
---
+++ Maxisoft.Utils/Math2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Maxisoft.Utils
+{
+    public static class Math2
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int NumberOfTrailingZeros(int x)
+        {
+            const int numbit = sizeof(int) * 8;
+            unchecked
+            {
+                if (x <= 0) return x < 0 ? 0 : numbit;
+                var res = (int) NumberOfTrailingZeros((ulong) x) - numbit;
+                return res;
+            }
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint NumberOfTrailingZeros(uint x)
+        {
+            const int numbit = sizeof(uint) * 8;
+            unchecked
+            {
+                var res = (uint) NumberOfTrailingZeros((ulong) x) - numbit;
+                return res;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long NumberOfTrailingZeros(long x)
+        {
+            unchecked
+            {
+                if (x <= 0) return x < 0 ? 0 : (sizeof(long) * 8);
+                var res = (long) NumberOfTrailingZeros((ulong) x);
+                return res;
+            }
+        }
+        public static ulong NumberOfTrailingZeros(ulong i)
+        {
+            unchecked
+            {
+                const ulong log2Of64 = 6;
+                const ulong numbit = sizeof(ulong) * 8;
+                if (i == 0) return numbit;
+                var n = numbit - 1;
+                ulong y;
+                for (ulong j = 0; j < log2Of64; j++)
+                {
+                    var power = (int) (numbit >> (int) (j + 1));
+
+                    y = i << power;
+                    if (y != 0)
+                    {
+                        n -= (ulong) power;
+                        i = y;
+                    }
+                }
+
+                return n - ((i << 1) >> (int) (numbit - 1));
+            }
+        }
+    }
+}
