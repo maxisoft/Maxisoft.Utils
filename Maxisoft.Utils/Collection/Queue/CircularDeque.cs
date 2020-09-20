d327177a8dccc31afd2ed8e31d5102fd86c41f82
maxisoft <maxisoft.eu@gmail.com>
2020-09-20 14:59:11 +0200

    Added a CircularDeque class

---
+++ Maxisoft.Utils/Collection/Queue/CircularDeque.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maxisoft.Utils.Collection.Queue
+{
+    [Serializable]
+    public class CircularDeque<T> : BaseCircularDeque<T, Deque<T>>, IList<T>, IReadOnlyList<T>
+    {
+        internal CircularDeque(Deque<T> deque, long capacity) : base(deque, capacity)
+        {
+        }
+
+        public CircularDeque(long cappedSize) : this(new Deque<T>(OptimalChunkSize(cappedSize)), cappedSize)
+        {
+        }
+
+        public int IndexOf(T item)
+        {
+            return Deque.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            Deque.Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            Deque.RemoveAt(index);
+        }
+
+        public T this[int index]
+        {
+            get => Deque[index];
+            set => Deque[index] = value;
+        }
+
+        internal static long OptimalChunkSize(long cappedSize)
+        {
+            return BoundedDeque<T>.OptimalChunkSize(cappedSize);
+        }
+    }
+}
---
+++ Maxisoft.Utils/Collection/Queue/ICircularDeque.cs
@@ -0,0 +1,8 @@
+﻿namespace Maxisoft.Utils.Collection.Queue
+{
+    public interface ICircularDeque<T> : IDeque<T>
+    {
+        bool IsFull { get; }
+        long Capacity { get; }
+    }
+}
