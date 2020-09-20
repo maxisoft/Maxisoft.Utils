0c8c0a16dd425eb161cb2c16c77b2fbf1bc98a48
maxisoft <maxisoft.eu@gmail.com>
2020-09-20 10:38:25 +0200

    Added PointerHelper a helper containing the function to get an object pointer

---
+++ Maxisoft.Utils.Tests/Objects/PointerHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Maxisoft.Utils.Objects;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Objects
+{
+    public class PointerHelperTests
+    {
+        [Fact]
+        public unsafe void Test_GetPointer()
+        {
+            var obj = new object();
+            var @ref = __makeref(obj);
+            var expected = **(IntPtr**) (&@ref);
+
+            var actual = PointerHelper.GetPointer(obj);
+            Assert.NotEqual(IntPtr.Zero, actual);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public unsafe void Test_GetPointer_Null()
+        {
+            object obj = null;
+            var @ref = __makeref(obj);
+            var expected = **(IntPtr**) (&@ref);
+
+            var actual = PointerHelper.GetPointer(obj);
+            Assert.Equal(IntPtr.Zero, actual);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Maxisoft.Utils/Objects/PointerHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Maxisoft.Utils.Objects
+{
+    public static class PointerHelper
+    {
+        public static IntPtr GetPointer<T>(in T? obj) where T : class
+        {
+            return new ObjectUnion {Object = obj};
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct ObjectUnion
+        {
+            [FieldOffset(0)] internal Boxed<object?> Object;
+            [FieldOffset(0)] private readonly Boxed<IntPtr> IntPtr;
+
+            public static implicit operator IntPtr(ObjectUnion union)
+            {
+                return union.IntPtr;
+            }
+        }
+    }
+}
