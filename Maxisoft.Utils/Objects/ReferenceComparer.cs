8b74f58c92083769b13b7cd4770b148aba31a67b
maxisoft <maxisoft.eu@gmail.com>
2020-09-20 10:41:02 +0200

    Added reference (aka object pointer) based Comparer & EqualityComparer

---
+++ Maxisoft.Utils/Objects/ReferenceComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Maxisoft.Utils.Objects
+{
+    public readonly struct ReferenceComparer<T> : IComparer<T> where T : class
+    {
+        public int Compare(T x, T y)
+        {
+            return PointerHelper.GetPointer(x).ToInt64().CompareTo(PointerHelper.GetPointer(y).ToInt64());
+        }
+    }
+}
---
+++ Maxisoft.Utils/Objects/ReferenceEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Maxisoft.Utils.Objects
+{
+    public struct ReferenceEqualityComparer<T> : IEqualityComparer<T> where T : class
+    {
+        public bool Equals(T x, T y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return EqualityComparer<T>.Default.GetHashCode(obj);
+        }
+    }
+}
