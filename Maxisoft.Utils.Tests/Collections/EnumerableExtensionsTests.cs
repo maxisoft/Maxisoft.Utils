f48880cc16bf67ee6384902644918466a9e0eafd
maxisoft <maxisoft.eu@gmail.com>
2020-09-28 22:16:37 +0200

    Added EnumerableExtensions Tests

---
+++ Maxisoft.Utils.Tests/Collections/EnumerableExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Maxisoft.Utils.Algorithms;
+using Maxisoft.Utils.Collections;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Collections
+{
+    public class EnumerableExtensionsTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(64)]
+        public void Test_ToDeque(int count)
+        {
+            var enumerable = Enumerable.Range(0, count).Select(Fibonacci.Fibo).ToImmutableList();
+            var res = enumerable.ToDeque();
+            Assert.Equal(enumerable, res);
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(64)]
+        public void Test_ToLinkedList(int count)
+        {
+            var enumerable = Enumerable.Range(0, count).Select(Fibonacci.Fibo).ToImmutableList();
+            var res = enumerable.ToLinkedList();
+            Assert.Equal(enumerable, res);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(64)]
+        public void Test_ToArrayList(int count)
+        {
+            var enumerable = Enumerable.Range(0, count).Select(Fibonacci.Fibo).ToImmutableList();
+            Assert.Equal(enumerable, ((IReadOnlyCollection<long>) enumerable).ToArrayList());
+            Assert.Equal(enumerable, ((ICollection<long>) enumerable).ToArrayList());
+            Assert.Equal(enumerable, ((IEnumerable<long>) enumerable).ToArrayList());
+            Assert.Equal(enumerable, enumerable.ToArray().ToArrayList());
+        }
+    }
+}
