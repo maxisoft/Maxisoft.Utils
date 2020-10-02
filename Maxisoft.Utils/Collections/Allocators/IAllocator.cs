87b36317d1db98f989a1cf44d2dd8b7b865bf2ec
maxisoft <maxisoft.eu@gmail.com>
2020-10-02 15:17:46 +0200

    Allocator implementation

---
+++ Maxisoft.Utils/Collections/Allocators/IAllocator.cs
@@ -0,0 +1,11 @@
+﻿namespace Maxisoft.Utils.Collections.Allocators
+{
+    public interface IAllocator<T>
+    {
+        T[] Alloc(ref int capacity);
+
+        void Free(ref T[] array);
+
+        void ReAlloc(ref T[] array, ref int capacity);
+    }
+}
---
+++ Maxisoft.Utils/Collections/Allocators/PooledAllocator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Buffers;
+
+namespace Maxisoft.Utils.Collections.Allocators
+{
+    public class PooledAllocator<T> : IAllocator<T>
+    {
+        public static readonly T[] EmptyArray = Array.Empty<T>();
+
+        public static readonly ArrayPool<T> DefaultPool = ArrayPool<T>.Shared;
+
+        public readonly ArrayPool<T> ArrayPool;
+
+        public PooledAllocator(ArrayPool<T>? arrayPool = null)
+        {
+            ArrayPool = arrayPool ?? DefaultPool;
+        }
+
+        public T[] Alloc(ref int capacity)
+        {
+            return capacity == 0 ? EmptyArray : ArrayPool.Rent(capacity);
+        }
+
+        public void Free(ref T[] array)
+        {
+            if (!ReferenceEquals(array, EmptyArray))
+            {
+                ArrayPool.Return(array);
+            }
+        }
+
+        public void ReAlloc(ref T[] array, ref int capacity)
+        {
+            if (capacity == array.Length)
+            {
+                return;
+            }
+
+            var old = array;
+            try
+            {
+                if (capacity == 0)
+                {
+                    array = EmptyArray;
+                }
+                else
+                {
+                    var oldCapacity = array.Length;
+                    array = Alloc(ref capacity);
+                    Array.Copy(old, array, Math.Min(oldCapacity, array.Length));
+                }
+            }
+            finally
+            {
+                Free(ref old);
+            }
+        }
+    }
+}
