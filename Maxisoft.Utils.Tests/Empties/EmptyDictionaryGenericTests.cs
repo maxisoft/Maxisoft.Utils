5671c9b0240383041758f24113436d767ce122cb
maxisoft <maxisoft.eu@gmail.com>
2020-10-06 10:33:34 +0200

    Added test for EmptyDictionary

---
+++ Maxisoft.Utils.Tests/Empties/EmptyDictionaryGenericTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Maxisoft.Utils.Empties;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Empties
+{
+    public class EmptyDictionaryGenericTests
+    {
+        [Theory, AutoData]
+        public void TestAdd(string key, string value)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.Throws<InvalidOperationException>(() => dict.Add(key, value));
+        }
+        
+        [Fact]
+        public void TestEnumerate()
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.Empty(dict);
+
+            foreach (var _ in dict)
+            {
+                Assert.False(true);
+            }
+        }
+        
+        [Fact]
+        public void TestClear()
+        {
+            var dict = new EmptyDictionary<string, string>();
+            dict.Clear();
+        }
+        
+        [Theory, AutoData]
+        public void TestContains(string key, string value)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.DoesNotContain(new KeyValuePair<string, string>(key, value), dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestContainsKey(string key)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.False(dict.ContainsKey(key));
+        }
+        
+        [Theory, AutoData]
+        public void TestTryGetValue(string key)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.Throws<InvalidOperationException>(() => dict.TryGetValue(key, out var _));
+        }
+        
+        [Theory, AutoData]
+        public void TestCopyTo([Range(0, 16)]int arrayLength)
+        {
+            var fixture = new Fixture();
+            var array = new KeyValuePair<string, string>[arrayLength];
+            for (var i = 0; i < arrayLength; i++)
+            {
+                array[i] = fixture.Create<KeyValuePair<string, string>>();
+            }
+
+            var copy = array.ToImmutableList();
+            var dict = new EmptyDictionary<string, string>();
+            dict.CopyTo(array, fixture.Create<int>());
+            Assert.Equal(copy, array);
+        }
+        
+        [Theory, AutoData]
+        public void TestRemove(string key)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            dict.Remove(key);
+            Assert.Empty(dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestIndexer(string key, string value)
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.Throws<InvalidOperationException>(() => dict[key]);
+            Assert.Throws<InvalidOperationException>(() => dict[key] = value);
+        }
+        
+        [Fact]
+        public void TestProperties()
+        {
+            var dict = new EmptyDictionary<string, string>();
+            Assert.False(dict.IsReadOnly);
+            Assert.IsType<EmptyCollection<string>>(dict.Keys);
+            Assert.IsType<EmptyCollection<string>>(dict.Values);
+            // ReSharper disable once xUnit2013
+            Assert.Equal(0, dict.Count);
+        }
+    }
+}
