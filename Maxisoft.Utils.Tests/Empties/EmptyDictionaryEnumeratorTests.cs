1b7b695e8e65228fbd7de9804e495439109969a4
maxisoft <maxisoft.eu@gmail.com>
2020-10-06 10:56:00 +0200

    More tests for empty dictionary

---
+++ Maxisoft.Utils.Tests/Empties/EmptyDictionaryEnumeratorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Maxisoft.Utils.Empties;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Empties
+{
+    public class EmptyDictionaryEnumeratorTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            var enumerator = new EmptyDictionaryEnumerator();
+            enumerator.Reset();
+            Assert.Throws<InvalidOperationException>(() => enumerator.Current);
+            Assert.Throws<InvalidOperationException>(() => enumerator.Entry);
+            Assert.Throws<InvalidOperationException>(() => enumerator.Key);
+            Assert.Throws<InvalidOperationException>(() => enumerator.Value);
+        }
+    }
+}
