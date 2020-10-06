7bbdb9c86d3fcc0541aab93b130bc34e2d830b48
maxisoft <maxisoft.eu@gmail.com>
2020-10-06 18:04:01 +0200

    Added NopDictionary tests

---
+++ Maxisoft.Utils.Tests/Empties/NoOpDictionaryGenericTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Maxisoft.Utils.Empties;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Empties
+{
+    public class NoOpDictionaryGenericTests
+    {
+        [Theory, AutoData]
+        public void TestAdd(string key, string value)
+        {
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var dict = new NoOpDictionary<string, string>();
+            dict.Add(key, value);
+            dict.Add(new KeyValuePair<string, string>(key, value));
+            Assert.Empty(dict);
+        }
+        
+        [Fact]
+        public void TestEnumerate()
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.Empty(dict);
+
+            foreach (var _ in dict)
+            {
+                Assert.False(true);
+            }
+        }
+        
+        [Fact]
+        public void TestClear()
+        {
+            var dict = new NoOpDictionary<string, string>();
+            dict.Clear();
+            Assert.Empty(dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestContains(string key, string value)
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.DoesNotContain(new KeyValuePair<string, string>(key, value), dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestContainsKey(string key)
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.False(dict.ContainsKey(key));
+        }
+        
+        [Theory, AutoData]
+        public void TestTryGetValue(string key)
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.Equal(default, dict.TryGetValue(key, out var _));
+        }
+        
+        [Theory, AutoData]
+        public void TestCopyTo([Range(0, 16)]int arrayLength)
+        {
+            var fixture = new Fixture();
+            var array = new KeyValuePair<string, string>[arrayLength];
+            for (var i = 0; i < arrayLength; i++)
+            {
+                array[i] = fixture.Create<KeyValuePair<string, string>>();
+            }
+
+            var copy = array.ToImmutableList();
+            var dict = new NoOpDictionary<string, string>();
+            dict.CopyTo(array, fixture.Create<int>());
+            Assert.Equal(copy, array);
+        }
+        
+        [Theory, AutoData]
+        public void TestRemove(string key, string value)
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.False(dict.Remove(key));
+            Assert.False(dict.Remove(new KeyValuePair<string, string>(key, value)));
+            Assert.Empty(dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestIndexer(string key, string value)
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.Equal(default, dict[key]);
+            dict[key] = value;
+            Assert.Empty(dict);
+        }
+        
+        [Fact]
+        public void TestProperties()
+        {
+            var dict = new NoOpDictionary<string, string>();
+            Assert.False(dict.IsReadOnly);
+            Assert.IsType<EmptyCollection<string>>(dict.Keys);
+            Assert.IsType<EmptyCollection<string>>(dict.Values);
+            // ReSharper disable once xUnit2013
+            Assert.Equal(0, dict.Count);
+        }
+    }
+}
---
+++ Maxisoft.Utils.Tests/Empties/NoOpDictionaryTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Maxisoft.Utils.Empties;
+using Xunit;
+
+namespace Maxisoft.Utils.Tests.Empties
+{
+    public class NoOpDictionaryTests
+    {
+        [Theory, AutoData]
+        public void TestAdd(string key, string value)
+        {
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var dict = new NoOpDictionary();
+            dict.Add(key, value);
+            Assert.Empty(dict);
+        }
+        
+        [Fact]
+        public void TestEnumerate()
+        {
+            var dict = new NoOpDictionary();
+            Assert.Empty(dict);
+
+            foreach (var _ in dict)
+            {
+                Assert.False(true);
+            }
+        }
+        
+        [Fact]
+        public void TestClear()
+        {
+            var dict = new NoOpDictionary();
+            dict.Clear();
+            Assert.Empty(dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestContains(string key)
+        {
+            var dict = new NoOpDictionary();
+            Assert.False(dict.Contains(key));
+        }
+        
+        [Theory, AutoData]
+        public void TestCopyTo([Range(0, 16)]int arrayLength)
+        {
+            var fixture = new Fixture();
+            var array = new object[arrayLength];
+            for (var i = 0; i < arrayLength; i++)
+            {
+                array[i] = fixture.Create<object>();
+            }
+            var copy = array.ToImmutableList();
+            var dict = new NoOpDictionary();
+            dict.CopyTo(array, fixture.Create<int>());
+            Assert.Equal(copy, array);
+        }
+        
+        [Theory, AutoData]
+        public void TestRemove(string key)
+        {
+            var dict = new NoOpDictionary();
+            dict.Remove(key);
+            Assert.Empty(dict);
+        }
+        
+        [Theory, AutoData]
+        public void TestIndexer(string key, string value)
+        {
+            var dict = new NoOpDictionary();
+            Assert.Equal(default, dict[key]);
+            dict[key] = value;
+            Assert.Empty(dict);
+        }
+        
+        [Fact]
+        public void TestProperties()
+        {
+            var dict = new NoOpDictionary();
+            Assert.False(dict.IsFixedSize);
+            Assert.False(dict.IsReadOnly);
+            Assert.IsType<EmptyCollection>(dict.Keys);
+            Assert.IsType<EmptyCollection>(dict.Values);
+            // ReSharper disable once xUnit2013
+            Assert.Equal(0, dict.Count);
+            Assert.False(dict.IsSynchronized);
+            // ReSharper disable once xUnit2005
+            Assert.IsType<NoOpDictionary>(dict.SyncRoot);
+        }
+    }
+}
