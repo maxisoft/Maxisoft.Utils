9c3bc735b074c0a48d13da458362e0f55c00c039
maxisoft <maxisoft.eu@gmail.com>
2020-10-06 17:33:03 +0200

    Added ArrayList Move and Swap methods ArrayList now use ref Indexer by default Formated code

---
+++ Maxisoft.Utils/Collections/Lists/ArrayList.AdditionalMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Maxisoft.Utils.Collections.Lists
+{
+    public abstract partial class ArrayList<T, TAllocator>
+    {
+        public void Resize(int size, bool clear = true)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), size, "negative");
+            }
+
+            var prevSize = Count;
+            EnsureCapacity(size);
+            Count = size;
+            if (clear && size < prevSize)
+            {
+                Array.Clear(_array, size, prevSize - size);
+            }
+        }
+
+        public void Move(int fromIndex, int toIndex)
+        {
+            CheckForOutOfBounds(fromIndex, nameof(fromIndex));
+            CheckForOutOfBounds(toIndex, nameof(toIndex));
+            if (fromIndex == toIndex)
+            {
+                return;
+            }
+
+            var tmp = this[fromIndex];
+            if (fromIndex < toIndex)
+            {
+                GetSlice(fromIndex + 1, toIndex - fromIndex).CopyTo(GetSlice(fromIndex, toIndex - fromIndex));
+            }
+            else
+            {
+                GetSlice(toIndex, fromIndex - toIndex).CopyTo(GetSlice(toIndex + 1, fromIndex - toIndex));
+            }
+
+            this[toIndex] = tmp;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Swap(int left, int right)
+        {
+            var tmp = this[left];
+            this[left] = this[right];
+            this[right] = tmp;
+        }
+    }
+}
