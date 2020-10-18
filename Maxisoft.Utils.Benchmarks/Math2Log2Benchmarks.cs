23e83cbf9d47f1fba6b1aae2e5014b214e0c869c
maxisoft <maxisoft.eu@gmail.com>
2020-10-18 15:42:33 +0200

    Math2 now support Log2 and NumberOfLeadingZeros methods

---
+++ Maxisoft.Utils.Benchmarks/Math2Log2Benchmarks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using Troschuetz.Random;
+
+namespace Maxisoft.Utils.Benchmarks
+{
+    public class Math2Log2Benchmarks
+    {
+        private List<double> floatingPoints;
+        private List<ulong> values;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            const int n = 1024 * 1024;
+            floatingPoints = new List<double>(n);
+            values = new List<ulong>(n);
+            var random = new TRandom(n);
+            for (var i = 0; i < n; i++)
+            {
+                var val = random.Next();
+                floatingPoints.Add(val);
+                values.Add((ulong) val);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public double Math_Log()
+        {
+            // Note the following add 1 float operation for each element
+            return floatingPoints.Aggregate(0.0, (current, f) => current * Math.Log(f, 2));
+        }
+
+        [Benchmark]
+        public ulong Math2_Log2()
+        {
+            // Note the following add 1 long operation for each element
+            return values.Aggregate(0UL, (current, f) => current * Math2.Log2(f));
+        }
+    }
+}
--- Maxisoft.Utils.Tests/Math2Tests.cs
+++ Maxisoft.Utils.Tests/Math2Tests.cs
@@ -1,28 +1,43 @@
-﻿using Xunit;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Maxisoft.Utils.Random;
+using Troschuetz.Random;
+using Xunit;
 
 namespace Maxisoft.Utils.Tests
 {
     public class Math2Tests
     {
-
         private static ulong NumberOfTrailingZerosSlow(ulong i)
         {
             checked
             {
-                if (i == 0) return 64;
-                if (i < 0) return 0;
+                if (i == 0)
+                {
+                    return 64;
+                }
+
+                if (i < 0)
+                {
+                    return 0;
+                }
+
                 ulong res = 0;
                 while ((i & 1) == 0)
                 {
-                    if (++res >= 64) return 64;
+                    if (++res >= 64)
+                    {
+                        return 64;
+                    }
+
                     i >>= 1;
                 }
 
                 return res;
             }
-            
         }
-        
+
         [Theory]
         [InlineData(0b010, 1)]
         [InlineData(0b011, 0)]
@@ -33,6 +48,302 @@ namespace Maxisoft.Utils.Tests
         {
             Assert.Equal(expected, Math2.NumberOfTrailingZeros(x));
             Assert.Equal(NumberOfTrailingZerosSlow(x), Math2.NumberOfTrailingZeros(x));
+            Assert.Equal(BitOperations.TrailingZeroCount(x), (int) Math2.NumberOfTrailingZeros(x));
+        }
+
+        [Theory]
+        [ClassData(typeof(RandomSeedGenerator))]
+        public void TestNumberOfTrailingZerosRandomFuzz(int seed)
+        {
+            var random = new TRandom(seed);
+            const int numIter = 1024;
+            for (var i = 0; i < numIter; i++)
+            {
+                var x = ((ulong) random.Next() << 48) ^ ((ulong) random.Next() << 16) ^ (ulong) random.Next();
+
+                // check for ulong
+                {
+                    var expected = BitOperations.TrailingZeroCount(x);
+                    checked
+                    {
+                        Assert.Equal((ulong) expected, Math2.NumberOfTrailingZeros(x));
+                    }
+                }
+
+                // check for signed long
+                {
+                    if ((long) x < 0)
+                    {
+                        Assert.Equal(64, Math2.NumberOfTrailingZeros((long) x));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.TrailingZeroCount(x);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.NumberOfTrailingZeros((long) x));
+                        }
+                    }
+                }
+
+                var xi = unchecked((uint) x);
+
+                // check for uint
+                {
+                    var expected = BitOperations.TrailingZeroCount(xi);
+                    checked
+                    {
+                        Assert.Equal((uint) expected, Math2.NumberOfTrailingZeros(xi));
+                    }
+                }
+
+                // check for signed int
+                {
+                    if ((int) xi < 0)
+                    {
+                        Assert.Equal(32, Math2.NumberOfTrailingZeros((int) xi));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.TrailingZeroCount(xi);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.NumberOfTrailingZeros((int) xi));
+                        }
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(1U << 4)]
+        [InlineData(1U << 8)]
+        [InlineData(1U << 16)]
+        [InlineData(1U << 31)]
+        [InlineData(1UL << 32)]
+        [InlineData(1UL << 63)]
+        [InlineData(uint.MinValue)]
+        [InlineData(uint.MaxValue)]
+        [InlineData(uint.MaxValue - 1)]
+        [InlineData(uint.MaxValue - 2)]
+        [InlineData(ulong.MinValue)]
+        [InlineData(ulong.MaxValue)]
+        [InlineData(ulong.MaxValue - 1)]
+        [InlineData(ulong.MaxValue - 2)]
+        public void Test_NumberOfLeadingZeros(ulong value)
+        {
+            {
+                var expected = BitOperations.LeadingZeroCount(value);
+                Assert.Equal((ulong) expected, Math2.NumberOfLeadingZeros(value));
+            }
+
+            {
+                var expected = BitOperations.LeadingZeroCount((uint) value);
+                Assert.Equal((uint) expected, Math2.NumberOfLeadingZeros((uint) value));
+            }
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(1U << 4)]
+        [InlineData(1U << 8)]
+        [InlineData(1U << 16)]
+        [InlineData(1U << 31)]
+        [InlineData(1UL << 32)]
+        [InlineData(1UL << 63)]
+        [InlineData(uint.MinValue)]
+        [InlineData(uint.MaxValue)]
+        [InlineData(uint.MaxValue - 1)]
+        [InlineData(uint.MaxValue - 2)]
+        [InlineData(ulong.MinValue)]
+        [InlineData(ulong.MaxValue)]
+        [InlineData(ulong.MaxValue - 1)]
+        [InlineData(ulong.MaxValue - 2)]
+        public void Test_NumberOfTrailingZeros(ulong value)
+        {
+            {
+                var expected = BitOperations.TrailingZeroCount(value);
+                Assert.Equal((ulong) expected, Math2.NumberOfTrailingZeros(value));
+            }
+
+            {
+                var expected = BitOperations.TrailingZeroCount((uint) value);
+                Assert.Equal((uint) expected, Math2.NumberOfTrailingZeros((uint) value));
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(RandomSeedGenerator))]
+        public void TestNumberOfLeadingZerosRandomFuzz(int seed)
+        {
+            var random = new TRandom(seed);
+            const int numIter = 1024;
+            for (var i = 0; i < numIter; i++)
+            {
+                var x = ((ulong) random.Next() << 48) ^ ((ulong) random.Next() << 16) ^ (ulong) random.Next();
+
+                // check for ulong
+                {
+                    var expected = BitOperations.LeadingZeroCount(x);
+                    checked
+                    {
+                        Assert.Equal((ulong) expected, Math2.NumberOfLeadingZeros(x));
+                    }
+                }
+
+                // check for signed long
+                {
+                    if ((long) x < 0)
+                    {
+                        Assert.Equal(0, Math2.NumberOfLeadingZeros((long) x));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.LeadingZeroCount(x);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.NumberOfLeadingZeros((long) x));
+                        }
+                    }
+                }
+
+                var xi = unchecked((uint) x);
+
+                // check for uint
+                {
+                    var expected = BitOperations.LeadingZeroCount(xi);
+                    checked
+                    {
+                        Assert.Equal((uint) expected, Math2.NumberOfLeadingZeros(xi));
+                    }
+                }
+
+                // check for signed int
+                {
+                    if ((int) xi < 0)
+                    {
+                        Assert.Equal(0, Math2.NumberOfLeadingZeros((int) xi));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.LeadingZeroCount(xi);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.NumberOfLeadingZeros((int) xi));
+                        }
+                    }
+                }
+            }
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(1U << 4)]
+        [InlineData(1U << 8)]
+        [InlineData(1U << 16)]
+        [InlineData(1U << 31)]
+        [InlineData(uint.MinValue)]
+        [InlineData(uint.MaxValue)]
+        public void Test_Log2(uint value)
+        {
+            var expected = BitOperations.Log2(value);
+            Assert.Equal((uint) expected, Math2.Log2(value));
+        }
+
+        [Theory]
+        [ClassData(typeof(RandomSeedGenerator))]
+        public void Test_Log2_RandomFuzz(int seed)
+        {
+            var random = new TRandom(seed);
+            const int numIter = 1024;
+            for (var i = 0; i < numIter; i++)
+            {
+                var x = ((ulong) random.Next() << 48) ^ ((ulong) random.Next() << 16) ^ (ulong) random.Next();
+
+                // check for ulong
+                {
+                    var expected = BitOperations.Log2(x);
+                    checked
+                    {
+                        Assert.Equal((ulong) expected, Math2.Log2(x));
+                    }
+                }
+
+                // check for signed long
+                {
+                    if ((long) x < 0)
+                    {
+                        Assert.Equal(0, Math2.Log2((long) x));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.Log2(x);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.Log2((long) x));
+                        }
+                    }
+                }
+
+                var xi = unchecked((uint) x);
+
+                // check for uint
+                {
+                    var expected = BitOperations.Log2(xi);
+                    checked
+                    {
+                        Assert.Equal((uint) expected, Math2.Log2(xi));
+                    }
+                }
+
+                // check for signed int
+                {
+                    if ((int) xi < 0)
+                    {
+                        Assert.Equal(0, Math2.Log2((int) xi));
+                    }
+                    else
+                    {
+                        var expected = BitOperations.Log2(xi);
+                        checked
+                        {
+                            Assert.Equal(expected, Math2.Log2((int) xi));
+                        }
+                    }
+                }
+            }
+        }
+
+
+        internal class RandomSeedGenerator : IEnumerable<object[]>
+        {
+            internal virtual RandomThreadSafe Random { get; } = new RandomThreadSafe();
+            internal virtual int NumberOfGen => 64;
+
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                for (var i = 0; i < NumberOfGen; i++)
+                {
+                    yield return new object[] {Random.Next()};
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
         }
     }
 }
